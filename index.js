fce9fb7eee9a3204dfc4ae704d0190aebf719e6c
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-29 11:59:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,9 +58,6 @@ function removeFromCart(item) {
       cart.splice(i,1);
       return cart;
     }
-    if(item !== cart[i].itemName){
-      return "That item is not in your cart.";
-    }
   }
   for(var i =0; i < cart.length; i++){
     if(item !== cart[i].itemName){
